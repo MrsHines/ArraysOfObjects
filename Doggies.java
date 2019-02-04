5759d1aa42b79beaae4a07bcc62aed4be219f105
MrsHines <34042849+MrsHines@users.noreply.github.com>
2019-02-04 08:51:01 -0600

    Starter Files

---
+++ Doggies.java
@@ -0,0 +1,38 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Arrays;
+
+public class Doggies
+{
+	private Dog[] pups;
+
+	public Doggies(int size)
+	{
+		pups = new Dog[size];
+	}
+	
+	public void add(int spot, int age, String name)
+	{
+		if(spot<pups.length)
+			pups[spot]= new Dog(age, name);
+	}
+
+	public String getNameOfOldest()
+	{
+		return null;
+	}
+
+	public String getNameOfYoungest()
+	{
+		return null;
+	}
+
+	public String toString()
+	{
+		return ""+Arrays.toString(pups);
+	}
+}
